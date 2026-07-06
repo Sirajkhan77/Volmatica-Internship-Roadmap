be46a28085fd8b96a6c12f61cf788e31178bbe8f
Sirajkhan77 <sirajyounis890@gmail.com>
2026-07-06 23:37:31 +0500

    day2 commits

---
+++ Week1/Day2/DataTypes.java
@@ -0,0 +1,11 @@
+package Week1.Day2;
+
+public class DataTypes {
+    int x =5;
+    int y =3;
+    int product = x*y;
+    public static void main(String[] args) {
+        DataTypes dt = new DataTypes();
+        System.out.println(dt.product);
+    }
+}
