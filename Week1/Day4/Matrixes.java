2b971c929b19961371b54acf549981721f0ba61c
Sirajkhan77 <sirajyounis890@gmail.com>
2026-07-09 01:23:06 +0500

    4th day commit

---
+++ Week1/Day4/Matrixes.java
@@ -0,0 +1,24 @@
+package Week1.Day4;
+
+public class Matrixes {
+    public static void main(String[] args) {
+        int x=1;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                System.out.print(x++ + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        
+
+    }
+}
