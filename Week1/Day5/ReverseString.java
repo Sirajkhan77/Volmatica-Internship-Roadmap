a3b240bc8a4ff2bb487ff799feb4bc507d57a1cd
Sirajkhan77 <sirajyounis890@gmail.com>
2026-07-09 23:11:11 +0500

    reversing the string

--- Week1/Day5/ReverseString.java
+++ Week1/Day5/ReverseString.java
@@ -1,4 +1,20 @@
 package Week1.Day5;
 
 public class ReverseString {
+    public void reverseString(char[] s) {
+        int left=0, right=s.length-1;
+        while (left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        char[] arr = {'S', 'i', 'r', 'A', 'j'};
+        ReverseString reverseString = new ReverseString();
+        reverseString.reverseString(arr);
+        System.out.println("Reversed String: " + Arrays.toString(arr));
+    }
 }
