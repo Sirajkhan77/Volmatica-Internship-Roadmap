da8642bce3f954fd1b0ed4e985cabc0e2209eff3
Sirajkhan77 <sirajyounis890@gmail.com>
2026-08-07 01:39:15 +0500

    week5, day1&2 tasks

---
+++ Week5/Day1/MavenDemo/src/main/java/com/SpringSiraj/library/Book.java
@@ -0,0 +1,14 @@
+package com.SpringSiraj.library;
+public class Book {
+
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public void display() {
+        System.out.println(title);
+    }
+
+}
---
+++ Week5/Day2/GradleDemo/src/main/java/com/SpringSiraj/library/Calculator.java
@@ -0,0 +1,8 @@
+package com.SpringSiraj.library;
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
